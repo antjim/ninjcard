e36ea97d7482c69f66a39f1f6ac26425ab2bf243
Mick <antji@gihub.com>
2019-08-12 13:01:42 +0200

    installation paths

--- utils/installACR122U.sh
+++ utils/installACR122U.sh
@@ -2,7 +2,24 @@
 
 sudo apt-get -y update
 sudo apt-get -y install libnfc-examples libnfc-dev libnfc-bin libusb-dev libpcsclite-dev
-tar -xvzf ./utils/libnfc-1.7.0-rc7.tar.gz
-./utils/libnfc-1.7.0-rc7/configure
-make -C ./utils/libnfc-1.7.0-rc7/
-make install -C ./utils/libnfc-1.7.0-rc7/
+tar -xvzf libnfc-1.7.0-rc7.tar.gz
+git clone https://github.com/nfc-tools/mfcuk.git
+git clone https://github.com/nfc-tools/mfoc.git
+
+cd libnfc-1.7.0-rc7
+./configure --prefix=/usr
+make && sudo make install
+
+cd ..
+cd mfcuk
+autoreconf -is
+./configure
+make && sudo make install
+
+cd ..
+cd mfoc
+autoreconf -is
+./configure
+make && sudo make install
+
+echo "[i] Completed."
--- utils/installRC522.sh
+++ utils/installRC522.sh
@@ -5,3 +5,5 @@ sudo apt-get -y install python-spidev python3-spidev
 git clone https://github.com/lthiery/SPI-Py.git
 git checkout 8cce26b9ee6e69eb041e9d5665944b88688fca68
 sudo python SPI-Py/setup.py install
+
+echo "[i] Completed."
